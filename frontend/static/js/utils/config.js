308b20d263e44d979b699d9b06f73e8bed023664
IrumShehryar <irum.shehryar@gmail.com>
2025-12-11 04:55:40 +0200

    changed localhost to apache folder

--- frontend/static/js/utils/config.js
+++ frontend/static/js/utils/config.js
@@ -2,7 +2,7 @@
  * Base API URL used by frontend services to build absolute endpoints.
  * @constant {string}
  */
-export const BASE_API_URL = "http://127.0.0.1:5000/api/v1"
+export const BASE_API_URL = "/revontulet/api/v1"
 
 /**
  * Build a full API URL for a given endpoint path.
