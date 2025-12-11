77dba9bdbd7f0088e9a8883db87ab1323c2cfac0
IrumShehryar <irum.shehryar@gmail.com>
2025-12-11 19:04:50 +0200

    added/revontulet to all redirects

--- frontend/static/js/cart.js
+++ frontend/static/js/cart.js
@@ -155,9 +155,9 @@ export function addToCart(item) {
 
   saveCartToStorage();
 
-  // Update cart total
- /* const totalPrice = cart.reduce((sum, i) => sum + (i.price || 0) * (i.quantity || 1), 0);
-  localStorage.setItem("cartTotal", totalPrice.toFixed(2));*/
+  // Update cart total (with safeguard for missing price)
+  const totalPrice = cart.reduce((sum, i) => sum + (i.price || 0) * (i.quantity || 1), 0);
+  localStorage.setItem("cartTotal", totalPrice.toFixed(2));
 
   showCartButton();
   showNotification(`${item.name} ${window.t && window.t.added_to_cart ? window.t.added_to_cart : 'added to cart!'}`);
